d04214679ce8790d4d60755a8d8c8452b3041e65
Tejash Desai <tejashdesai241@gmail.com>
2017-07-18 14:32:35 +0530

    added GIMP and few other basic software

--- ubuntu_libraries.sh
+++ ubuntu_libraries.sh
@@ -9,10 +9,21 @@ ls /usr/lib/jvm/ | grep 8-openjdk | { read java_home; export JAVA_HOME=$java_hom
 echo Downloading and Installing Nylas Mail Now...
 wget https://edgehill.nylas.com/download?platform=linux-deb
 dpkg -i ./download?platform=linux-deb
-sudo apt-get install -f
+apt-get install -f
 
 # Installing Sublime Text 3. Refer - http://tipsonubuntu.com/2017/05/30/install-sublime-text-3-ubuntu-16-04-official-way/
 echo Installing Sublime Text now...
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get install sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+apt-get install sublime-text
+
+# Python essentials
+apt-get install python-pip
+apt-get install virtualenv
+
+# More Ubuntu Software
+apt-get install vim
+# GIMP - refer to http://tipsonubuntu.com/2016/08/02/install-gimp-2-9-5-ubuntu-16-04/
+add-apt-repository ppa:otto-kesselgulasch/gimp-edge
+apt install gimp gimp-gmic
+apt install ppa-purge && ppa-purge ppa:otto-kesselgulasch/gimp-edge
