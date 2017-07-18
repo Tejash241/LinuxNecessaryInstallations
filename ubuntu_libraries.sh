1e345327a8551e4e04636388185860ecc999ef34
Tejash Desai <tejashdesai241@gmail.com>
2017-07-18 17:57:12 +0530

    Added -y for all install commands

--- ubuntu_libraries.sh
+++ ubuntu_libraries.sh
@@ -3,40 +3,45 @@ echo Installing Distributions and Basic Libraries Updates Now....
 apt-get update && apt-get upgrade && apt-get dist-upgrade
 
 echo Installing JDK8 Now......
-apt-get install openjdk-8-jdk
+apt-get install -y openjdk-8-jdk
 ls /usr/lib/jvm/ | grep 8-openjdk | { read java_home; export JAVA_HOME=$java_home; echo JAVA_HOME=$JAVA_HOME; }
 
 echo Downloading and Installing Nylas Mail Now...
 wget https://edgehill.nylas.com/download?platform=linux-deb
 dpkg -i ./download?platform=linux-deb
-apt-get install -f
+apt-get install -yf
 
 # Installing Sublime Text 3. Refer - http://tipsonubuntu.com/2017/05/30/install-sublime-text-3-ubuntu-16-04-official-way/
 echo Installing Sublime Text now...
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-apt-get install sublime-text
+apt-get install -y sublime-text
 
 # Python essentials
-apt-get install python-pip
-apt-get install virtualenv
+apt-get install -y python-pip
+apt-get install -y virtualenv
 
-apt-get update
 # More Ubuntu Software
-apt-get install vim
+apt-get install -y vim
 # GIMP - refer to http://tipsonubuntu.com/2016/08/02/install-gimp-2-9-5-ubuntu-16-04/
 add-apt-repository ppa:otto-kesselgulasch/gimp-edge
-apt install gimp gimp-gmic
-apt install ppa-purge && ppa-purge ppa:otto-kesselgulasch/gimp-edge
+apt-get update
+apt install -y gimp gimp-gmic
+apt install -y ppa-purge && ppa-purge ppa:otto-kesselgulasch/gimp-edge
 # Google Chrome - refer to https://askubuntu.com/questions/510056/how-to-install-google-chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get install google-chrome-stable
+apt-get install -y google-chrome-stable
 # Calendar indicator
-sudo add-apt-repository ppa:atareao/atareao
-sudo apt install calendar-indicator
+add-apt-repository ppa:atareao/atareao
+apt-get update
+apt install -y calendar-indicator
 # Go-gfor-it Refer - https://itsfoss.com/go-for-it-to-do-app-in-linux/
-sudo add-apt-repository ppa:mank319/go-for-it
-sudo apt-get install go-for-it
-
+add-apt-repository ppa:mank319/go-for-it
+apt-get update
+apt-get install -y go-for-it
+# F.lux
+add-apt-repository ppa:nathan-renniewaldock/flux
+apt-get update
+apt-get install -y fluxgui
 
